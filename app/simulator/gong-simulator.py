7400a2e30512c4ceb88aee041e149bf61440e0b4
lewuskip <piotr.lewandowski.uk@gmail.com>
2018-06-10 17:51:48 +0100

    Working simulator

--- app/simulator/gong-simulator.py
+++ app/simulator/gong-simulator.py
@@ -1,20 +1,43 @@
 #!/usr/bin/env python
 
+import socket
+import time
+import curses
 
 
 class Gong(object):
+    SN = 1
+    PORTS = [1001, 1002, 1003, 1004]
+    TRIES_QUANTITY = 3
+    TRIES_INTERVAL = 0.100
 
-    def __init__(self):
-        pass
+    def __init__(self, ipAddress):
+        self.ip = ipAddress
+        self.eventID = 1
 
+    def sendSeries(self, msg):
+        print (msg)
+        for port in Gong.PORTS:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+            sock.sendto(msg, (self.ip, port))
 
     def click(self):
-        print("Click")
+        for x in range(Gong.TRIES_QUANTITY):
+            msg = "event=click, seventID={},gongSN={}".format(self.eventID, Gong.SN)
+            time.sleep(Gong.TRIES_INTERVAL)
+            self.sendSeries(msg)
+        self.eventID += 1
 
 
 
 if __name__ == "__main__":
-    gong = Gong()
+    #gong = Gong("192.168.0.29")
+    gong = Gong("192.168.0.255")
+    while True:
+        time.sleep(3)
+        gong.click()
 
-    gong.click()
+
+
 
