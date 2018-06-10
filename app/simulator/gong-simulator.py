dffff23ec3a0e54d5e04714da33affbeacff39e6
lewuskip <piotr.lewandowski.uk@gmail.com>
2018-06-10 15:08:34 +0100

    Added gong simulator file template.

---
+++ app/simulator/gong-simulator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+
+
+class Gong(object):
+
+    def __init__(self):
+        pass
+
+
+    def click(self):
+        print("Click")
+
+
+
+if __name__ == "__main__":
+    gong = Gong()
+
+    gong.click()
+
